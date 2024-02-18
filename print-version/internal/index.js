c38e8b6c4e8b1443c12370428604f50a5ef9dbeb
cawfeecake <48775802+cawfeecake@users.noreply.github.com>
2024-02-18 13:17:35 +0000

    DRY internal code

--- print-version/internal/index.js
+++ print-version/internal/index.js
@@ -1,11 +1,11 @@
-console.log('::group::`process.arch`');
-console.log(
-    JSON.stringify(process.arch, null, 2)
-);
-console.log('::endgroup::');
+function printInGroup(title, toPrint) {
+    console.log(`::group::${title}`);
+    console.log(toPrint);
+    console.log('::endgroup::');
+}
 
-console.log('::group::`process.env`');
-console.log(
-    JSON.stringify(process.env, null, 2)
-);
-console.log('::endgroup::');
+printInGroup('`process.arch`', JSON.stringify(process.arch, null, 2))
+printInGroup('`process.argv`', JSON.stringify(process.argv, null, 2))
+printInGroup('`process.env`', JSON.stringify(process.env, null, 2))
+printInGroup('`process.version`', process.version)
+printInGroup('`process.versions`', JSON.stringify(process.versions, null, 2))
